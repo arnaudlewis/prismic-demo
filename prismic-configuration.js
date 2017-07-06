98b7b14fad76db97046f8994486bfd47be24f3f8
Arnaud Lewis <arnaud.lewis@gmail.com>
2017-07-06 16:22:54 +0200

    add i18n support

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -13,7 +13,7 @@ module.exports = {
   // This function will be used to generate links to Prismic.io documents
   // As your project grows, you should update this function according to your routes
   linkResolver: function(doc, ctx) {
-    if (doc.type == 'page') return `/page/${doc.uid}`;
+    if (doc.type == 'page') return `/${doc.lang}/page/${doc.uid}`;
     return '/';
   }
 };
